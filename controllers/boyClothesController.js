f4b020dac9c6a4b123480cd5a5dcd638d9148b61
MhmdFais <117585798+MhmdFais@users.noreply.github.com>
2024-09-11 21:37:42 +0530

    Add boyClothesController and boysCloth routes

---
+++ controllers/boyClothesController.js
@@ -0,0 +1,27 @@
+function get(req, res) {
+    // Display all the list
+}
+
+function viewAnItem(req, res) {
+    // Logic to view an individual item
+}
+
+function deleteAnItem(req, res) {
+    // Logic to delete an item
+}
+
+function editAnItem(req, res) {
+    // Logic to display the item for editing
+}
+
+function enterEditedItem(req, res) {
+    // Logic to handle the edited item submission
+}
+
+module.exports = {
+    get,
+    viewAnItem,
+    deleteAnItem,
+    editAnItem,
+    enterEditedItem
+};
---
+++ routes/boysCloth.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const boysClothesController = require('../controllers/boyClothesController')
+
+const boysRouter = express.Router()
+
+boysRouter.get('/', boysClothesController.get)
+boysRouter.get('/view-item/:id', boysClothesController.viewAnItem)
+boysRouter.delete('/view-item/:id/delete', boysClothesController.deleteAnItem)
+boysRouter.get('/view-item/:id/edit', boysClothesController.editAnItem)
+boysRouter.post('/view-item/:id/edit', boysClothesController.enterEditedItem)
+
+module.exports = {boysRouter}
